7fddd15c8bc1f6a78e4095794e67c42761e3b103
Jacob Friis Saxberg <jacob@webcom.dk>
2021-06-12 20:02:31 +0200

    fix: long first

--- src/transform.ts
+++ src/transform.ts
@@ -2,7 +2,7 @@ import { GeoJSON, Position } from 'geojson';
 import { Record } from './index.d';
 
 function transform(records: Record[] = []): GeoJSON {
-  const coordinates:Position[] = records.map(({ position_lat, position_long }) => [position_lat, position_long]);
+  const coordinates:Position[] = records.map(({ position_long, position_lat }) => [position_long, position_lat]);
   const featureCollection: GeoJSON = {
     type: 'FeatureCollection',
     features: [{
