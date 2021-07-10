196d19eac6ee09adaab73aa97e77ad300515a5f9
Jacob Friis Saxberg <jacob@webcom.dk>
2021-07-10 17:55:59 +0200

    fix for undefined coords

--- src/transform.ts
+++ src/transform.ts
@@ -5,13 +5,18 @@ function recordTimestampToISOString({ timestamp }: Record) {
   return timestamp.toISOString();
 }
 
+function recordFilter({ position_long, position_lat }: Record): boolean {
+  return [position_long, position_lat].some((val) => val === undefined || Number.isNaN(val)) === false;
+}
+
 function record({ position_long, position_lat, altitude }: Record): Position {
   return [position_long, position_lat, altitude];
 }
 
 function transform(records: Record[] = []): FeatureCollection {
-  const coordinates:Position[] = records.map(record);
-  const coordTimes = records.map(recordTimestampToISOString);
+  const recordsFiltered = records.filter(recordFilter);
+  const coordinates = recordsFiltered.map(record);
+  const coordTimes = recordsFiltered.map(recordTimestampToISOString);
   const [time] = coordTimes;
   return {
     type: 'FeatureCollection',
