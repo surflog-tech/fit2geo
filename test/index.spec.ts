6cf89189dbc8ea301bcdeca74d1b239a1a423d7a
Jacob Friis Saxberg <jacob@webcom.dk>
2021-07-24 18:22:31 +0200

    cleanup

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,6 +1,5 @@
 import { readFileSync } from 'fs';
 import parseFit from '../src/parse';
-// import simplify from '../src/simplify';
 import fit2geo from '../src/index';
 
 // const fitFile = './assets/6829812928_ACTIVITY.fit';
@@ -13,22 +12,6 @@ describe('fit2geo', () => {
     const fitData:ArrayBuffer = readFileSync(fitFile);
     return parseFit(fitData);
   });
-
-  // xit('should measure simplify', async function() {
-  //   this.timeout(10000);
-  //   const fitData:ArrayBuffer = readFileSync(fitFile);
-  //   const result = await fit2geo(fitData);
-  //   const { records } = await parseFit(fitData);
-  //   const resultSimplified = simplify(records);
-  //   // @ts-ignore
-  //   console.log(JSON.stringify(result.geometry.coordinates.reduce((a, v) => a += v.length, 0)));
-  //   // console.log(JSON.stringify(resultSimplified.geometry.coordinates.reduce((a, v) => a += v.length, 0)));
-  //   // @ts-ignore
-  //   console.log(JSON.stringify(resultSimplified.features.length));
-  //   // @ts-ignore
-  //   // console.log(JSON.stringify(resultSimplified.geometry.coordinates.reduce((a, v) => a += v.length, 0)));
-  //   // 7204
-  // });
 
   it('should log result', async function() {
     this.timeout(10000);
